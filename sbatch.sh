9f02fb8c8344c7f9b0cab0ca0afd3e41a09e2ba8
jdoelger <jdoelger@mit.edu>
2021-06-22 15:56:19 -0400

    cluster changes

--- sbatch.sh
+++ sbatch.sh
@@ -5,4 +5,6 @@
 #SBATCH --time=1-9:20:00
 #SBATCH --partition=sched_mit_arupc_long
 
+#SBATCH --mem-per-cpu=10gb
+
 python /home/jdoelger/InfluenzaFitnessInference/code/notebooks/fitnessinference/analysis.py

bba59d67a2d1c6528b60832669a61745e4017d45
jdoelger <jdoelger@mit.edu>
2021-06-23 10:29:10 -0400

    new simus

--- sbatch.sh
+++ sbatch.sh
@@ -5,6 +5,6 @@
 #SBATCH --time=1-9:20:00
 #SBATCH --partition=sched_mit_arupc_long
 
-#SBATCH --mem-per-cpu=10gb
+#SBATCH --mem-per-cpu=1gb
 
-python C:\Users\julia\Documents\Resources\InfluenzaFitnessLandscape\NewApproachFromMarch2021\InfluenzaFitnessInference\code\notebooks\fitnessinference\analysis.py
+python /home/jdoelger/InfluenzaFitnessInference/code/notebooks/fitnessinference/simulation.py
